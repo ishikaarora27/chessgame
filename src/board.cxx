10b86597ed1336398008c7ceb4f8ef0c0da920ed
ishikaarora27 <ishikaarora2025@u.northwestern.edu>
2022-12-01 19:58:21 -0600

    board changes

--- src/board.cxx
+++ src/board.cxx
@@ -15,7 +15,27 @@
 
 Board::Board()
 : grid[8][8] {
-    //add the chess pieces
+
+    for (int i = 0; i < 8; i++) {
+        grid[1][i] = new Pawn(Piece::Player::White, (1, i));
+        grid[6][i] = new Pawn(Piece::Player::Black, (6, i));
+    }
+    grid[0][0] = new Rook(Piece::Player::White, (0, 0));
+    grid[0][7] = new Rook(Piece::Player::White, (0, 7));
+    grid[7][0] = new Rook(Piece::Player::Black, (7, 0));
+    grid[7][7] = new Rook(Piece::Player::Black, (7, 7));
+    grid[0][1] = new Knight(Piece::Player::White, (0, 1));
+    grid[0][6] = new Knight(Piece::Player::White, (0, 6));
+    grid[7][1] = new Knight(Piece::Player::Black, (7, 1));
+    grid[7][6] = new Knight(Piece::Player::Black, (7, 6));
+    grid[0][2] = new Bishop(Piece::Player::White, (0, 2));
+    grid[0][5] = new Bishop(Piece::Player::White, (0, 5));
+    grid[7][2] = new Bishop(Piece::Player::Black, (7, 2));
+    grid[7][5] = new Bishop(Piece::Player::Black, (7, 5));
+    grid[0][3] = new Queen(Piece::Player::White,  (0, 3));
+    grid[7][3] = new Queen(Piece::Player::Black, (7, 3));
+    grid[0][4] = new King(Piece::Player::White, (0, 4));
+    grid[7][4] = new King(Piece::Player::Black, (7, 4));
 
 }
 
--- src/board.hxx
+++ src/board.hxx
@@ -22,22 +22,50 @@ class Board
 
 public:
 
+    using Position = ge211::Posn<int>;
+
     //Board constructor: creates a board and adds all the pieces in their starting positions
     Board();
 
     //returns the actual piece at a given position
-    Piece get_piece(ge211::Posn<int> pos);
+    Piece get_piece(Position pos);
 
     //returns the player at a given position
-    Player get_player(ge211::Posn<int> pos);
+    Player get_player(Position pos);
 
-    void board_set(ge211::Posn<int> pos, Piece p);
+    void board_set(Position pos, Piece p);
+    // applies move to board
+    // checks if move is valid with all possible moves
+    // checks if player is still in check after the move
+    // after successful move, current player is changed
+
+    bool move_piece(Move *move);
+    // promotes the piece(pawn)
+    void promote_piece(Move *move, Piece::PieceType type);
+    // undoes the last move
+    void undo_move();
+    // checks if piece is in check by:
+    // checking if one move can capture king
+    // returns true if the current player is in check
+    bool in_check() const;
+    // checks if piece is in checkmate by:
+    // applying each possible move to a copy of the board
+    // then checking if the piece is still in check after its move
+    // returns true if the player is in checkmate
+    bool in_checkmate() const;
+    // checks for a draw
+    // if there is no possible move for the current player
+    // returns true it is a draw
+    bool is_draw() const;
+    // checks if move is a promotion
+    // returns true if promotion
+    bool is_promotion(Move *move) const;
 
 
 private:
 
     //The 2D array of Tiles
-    Tile grid [][];
+    Piece grid [][];
 
 
 
